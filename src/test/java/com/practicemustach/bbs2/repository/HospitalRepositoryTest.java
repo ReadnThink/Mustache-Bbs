b8e79e691105be02caa9dfa2a24800a65667b175
solbae <sol4854@naver.com>
2022-11-15 00:06:21 +0900

    테스트 주석처리

--- src/test/java/com/practicemustach/bbs2/repository/HospitalRepositoryTest.java
+++ src/test/java/com/practicemustach/bbs2/repository/HospitalRepositoryTest.java
@@ -8,55 +8,55 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
-class HospitalRepositoryTest {
-
-    @Autowired
-    HospitalRepository hospitalRepository;
-
-    @Test
-    void findByBusinessNameIn() {
-        List<String> inClues = new ArrayList<>();
-        inClues.add("보건소");
-        inClues.add("보건지소");
-        inClues.add("보건진료소");
-
-        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
-        printHospitalNameAndAddress(hospitals);
-    }
-
-    @Test // like="%송파구%" containing = %안넣어도 된다.
-    void containing(){
-        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining("송파구");
-        printHospitalNameAndAddress(hospitals);
-    }
-
-    @Test
-    void startsWith(){
-        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressStartsWith("경희");
-        printHospitalNameAndAddress(hospitals);
-    }
-
-    @Test
-    void endsWith(){
-        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressEndingWith("병원");
-        printHospitalNameAndAddress(hospitals);
-    }
-
-    @Test
-    void between(){
-        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountBetween(10, 20);
-        //정렬하여 출력
-        List<Hospital> orderByHospitals = hospitalRepository.findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(10, 20);
+//@SpringBootTest
+//class HospitalRepositoryTest {
+//
+//    @Autowired
+//    HospitalRepository hospitalRepository;
+//
+//    @Test
+//    void findByBusinessNameIn() {
+//        List<String> inClues = new ArrayList<>();
+//        inClues.add("보건소");
+//        inClues.add("보건지소");
+//        inClues.add("보건진료소");
+//
+//        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
 //        printHospitalNameAndAddress(hospitals);
-        printHospitalNameAndAddress(orderByHospitals);
-    }
-
-    void printHospitalNameAndAddress(List<Hospital> hospitals) {
-        for (var hospital : hospitals) {
-            System.out.printf("%s | %s %f\n", hospital.getHospitalName(), hospital.getRoadNameAddress(), hospital.getTotalAreaSize());
-        }
-
-        System.out.println(hospitals.size());
-    }
-}
+//    }
+//
+//    @Test // like="%송파구%" containing = %안넣어도 된다.
+//    void containing(){
+//        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining("송파구");
+//        printHospitalNameAndAddress(hospitals);
+//    }
+//
+//    @Test
+//    void startsWith(){
+//        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressStartsWith("경희");
+//        printHospitalNameAndAddress(hospitals);
+//    }
+//
+//    @Test
+//    void endsWith(){
+//        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressEndingWith("병원");
+//        printHospitalNameAndAddress(hospitals);
+//    }
+//
+//    @Test
+//    void between(){
+//        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountBetween(10, 20);
+//        //정렬하여 출력
+//        List<Hospital> orderByHospitals = hospitalRepository.findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(10, 20);
+////        printHospitalNameAndAddress(hospitals);
+//        printHospitalNameAndAddress(orderByHospitals);
+//    }
+//
+//    void printHospitalNameAndAddress(List<Hospital> hospitals) {
+//        for (var hospital : hospitals) {
+//            System.out.printf("%s | %s %f\n", hospital.getHospitalName(), hospital.getRoadNameAddress(), hospital.getTotalAreaSize());
+//        }
+//
+//        System.out.println(hospitals.size());
+//    }
+//}
